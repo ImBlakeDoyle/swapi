d8da96cfaea09343e57bd0fdfec1ded81aa78cdf
Blake <blake.doyle@hotmail.com.au>
2019-08-20 20:10:38 -0700

    push movies to different array when favourited

--- src/App.js
+++ src/App.js
@@ -5,29 +5,52 @@ import axios from "axios";
 function App (){
 
     const [favourited, setFavourited] = useState([]);
-    const [allFilms, setFilms] = useState([{}]);
+    const [allFilms, setFilms] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get("https://swapi.co/api/films/")
             setFilms(response.data.results);
             console.log(response.data.results);
+            console.log(favourited);
         };
         fetchData();
     }, []);
 
     return(
         <div>
-            {(allFilms.length <= 1) ? <div>Loading...</div> : 
-            <ul>
-                {allFilms.map((film, index) => {
-                    return(
-                        <li key={index}>{film.title}</li>
-                    );
-                })}
-            </ul>
- }
-            
+            <div>
+            {(!allFilms) ? <div>Loading...</div> : 
+                <ul>
+                    {allFilms.map((film, index) => {
+                        return(
+                            <div key={index}>
+                                <li>{film.title}</li> 
+                                <button onClick={() => {
+                                    setFavourited([...favourited, film]);
+                                    let updatedFilms = allFilms;
+                                    updatedFilms.splice(index, 1);
+                                    setFilms(updatedFilms);
+                                }}>Favourite</button>
+                            </div>
+                        );
+                    })}
+                </ul>
+            }
+            </div>
+            <div>
+            {(!favourited.length) ? <div>No favs</div> :
+                <ul>
+                    {favourited.map((film, index) => {
+                        return(
+                            <div key={index}>
+                                <li>{film.title}</li>
+                            </div>
+                        )
+                    })}
+                </ul>
+            }
+            </div>
         </div>
     );
 }
