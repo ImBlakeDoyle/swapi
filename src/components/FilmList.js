0fa56d4aa234af4a72f50e255b1b226dadccb6a1
Blake <blake.doyle@hotmail.com.au>
2019-08-25 16:00:37 -0700

    have search properly filter and render film list

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import BarSearch from "./BarSearch";
 
 import { Link } from "react-router-dom";
-
-//make functional with state to hold filtered.
-//have method which sets filtered state
-//pass that method to searchbar component
 
 function FilmList(props){
 
@@ -33,19 +29,37 @@ function FilmList(props){
             query={query}/>
             <hr/>
 
-            {/* Loop through filtered array, if element in array
-                is also within allFilms, then do the below mapping
-                Otherwise don't render that element */}
-
             {query.filteredData.length > 0  && query.text.length > 1 ?
                 <ul>
                     {query.filteredData.map((film, index) => {
-                        return(
-                            <li key={index}>{film.title}</li>
-                        );
+                        if (film.favourite === true){
+                            return(
+                                <div key={index}>
+                                    <Link to={{
+                                        pathname: `/film/${film.title}`,
+                                        query:{
+                                            film: film,
+                                        }
+                                    }}><li>{film.title}</li></Link>
+                                    <button onClick={() => {props.removeFav(film, index)}}>Unfavourite</button>
+                                </div>
+                            );
+                        } else {
+                            return(
+                                <div key={index}>
+                                    <Link to={{
+                                        pathname: `/film/${film.title}`,
+                                        query:{
+                                            film: film,
+                                        }
+                                    }}><li>{film.title}</li></Link>
+                                    <button onClick={() => {props.addFav(film, index)}}>Favourite</button>
+                                </div> 
+                            );
+                        }
                     })} 
                 </ul> :
-                    <ul>
+                <ul>
                     {props.films.allFilms.map((film, index) => {
                         if (film.favourite === true){
                             return(
